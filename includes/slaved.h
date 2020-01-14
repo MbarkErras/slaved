1448c53a5d5c0f82df071534b08779faae0a36cf
M'bark Erras <mbarekerras@gmail.com>
2020-01-14 15:36:37 +0100

    coding pic primitives between slave and slave_program

--- includes/slaved.h
+++ includes/slaved.h
@@ -16,6 +16,15 @@
 
 # define PORT 1337
 # define LISTEN_QUEUE 1
+# define SLAVE_PROGRAM_NAME "slave_program"
+
+typedef struct  s_program
+{
+    pid_t       pid;
+    int         w_stdin;
+    int         r_stdout;
+}               t_program;
+
 
 typedef struct  s_request
 {
@@ -25,19 +34,21 @@ typedef struct  s_request
 }               t_request;
 
 # define PROGRAM 0
-void    execute_program_request(t_request *request);
+void    execute_program_request(t_slave *slave, t_request *request);
 
 # define COMPUTATION 1
-void    *execute_computation_request(t_request *request, size_t *response_size);
+void    *execute_computation_request(t_slave *slave, t_request *request, size_t *response_size);
 
 typedef struct  s_slave
 {
     int             connection_fd;
     t_dstruct_list  requests_queue;
+    t_program       program;
     char            flags;
 }               t_slave;
 
-# define ACCEPT_CONNECTION 0
+# define PROGRAM_RECEIVED 0
+# define ACCEPT_CONNECTION 1
 
 /*
 ** CONSTRUCTORS/DECONSTRUCTORS
@@ -59,5 +70,11 @@ void    destroy_request(void *request);
 */
 
 # define CAST(v, t) ((t)v)
+# define F_GET(x, f) (x & (1 << f))
+# define F_BGET(x, f) (x & f)
+# define F_SET(x, f) (x |= (1 << f))
+# define F_BSET(x, f) (x |= f)
+# define F_UNSET(x, f) (x &= ~(1 << f))
+# define F_BUNSET(x, f) (x &= ~f)
 
 #endif
--- srcs/request_execution.c
+++ srcs/request_execution.c
@@ -1,13 +1,40 @@
 #include "slaved.h"
 
-void    execute_program_request(t_request *request)
+void    execute_program_request(t_slave *slave, t_request *request)
 {
-    (void)request;
+    int fd;
+    int stdin_pipe[2];
+    int stdout_pipe[2];
+
+    if ((fd = open(SLAVE_PROGRAM_NAME, O_CREAT | O_WRONLY)) == -1)
+        return ;
+    if (pipe(stdin_pipe) == -1 || pipe(stdout_pipe) == -1)
+        return ;
+    slave->program.w_stdin = stdin_pipe[1];
+    slave->program.r_stdout = stdout_pipe[0];
+    write(fd, request->data, request->size);
+    if (!(slave->program.pid = fork()))
+    {
+        dup2(stdin_pipe[0], 0);
+        dup2(stdin_pipe[1], 1);
+        if (execve(SLAVE_PROGRAM_NAME, NULL, NULL) == -1)
+            exit (0);
+    }
+    else if (slave->program.pid == -1)
+        return ;
+    F_SET(slave->flags, PROGRAM_RECEIVED);
 }
 
-void    *execute_computation_request(t_request *request, size_t *response_size)
+void    *execute_computation_request(t_slave *slave, t_request *request, size_t *response_size)
 {
-    (void)request;
-    (void)response_size;
-    return (NULL);
+    void    *result;
+
+    if (!F_GET(slave->flags, PROGRAM_RECEIVED))
+        return (NULL);
+    write(slave->program.w_stdin, request->data, request->size);
+    sleep(1);
+    read(slave->program.r_stdout, response_size, sizeof(size_t));
+    result = malloc(sizeof(char) * *response_size);
+    read(slave->program.r_stdout, result, *response_size);
+    return (result);
 }
