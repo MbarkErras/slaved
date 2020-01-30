28e653b172a54d06384d5cd237d18f681618b7a5
M'bark Erras <mbarekerras@gmail.com>
2020-01-30 01:44:15 +0100

    debugging slave_routine

--- master_middlewares/sources/master_middlewares.c
+++ master_middlewares/sources/master_middlewares.c
@@ -23,6 +23,8 @@ int init_slaves(t_cluster *cluster)
 **      - if successful, update task accordingly.
 **      - if failed, enqueue task again to the global task queue. 
 */
+# define CAST(v, t) ((t)v)
+
 
 void    *slave_routine(void *slave)
 {
@@ -38,19 +40,35 @@ void    *slave_routine(void *slave)
             continue ;
         task = CAST(slave, t_slave *)->tasks_queue.head;
         DEBUG("\t\tpicking a task!..\n");
-        while (write_packet(CAST(slave, t_slave *)->socket, *CAST(task->content, t_task *)->request))
-            continue ;
-        err = read_packet(CAST(slave, t_slave *)->socket, &response);
-        printf("ERR: %d\n", err);
+        printf("%d %d\n", CAST(slave, t_slave *)->socket, CAST(task->content, t_task *)->request->type);
+        if (write_packet(CAST(slave, t_slave *)->socket, *(CAST(task->content, t_task *)->request)))
+        {
+            DEBUG("\nXXX\terror writing to master, exiting nodes thread\n");
+            pthread_exit(NULL);
+        }
+        if (read_packet(CAST(slave, t_slave *)->socket, &response))
+        {
+            DEBUG("\nXXX\terror reading to master, exiting nodes thread\n");
+            pthread_exit(NULL);
+        }
+        printf("sr 1\n");
         CAST(task->content, t_task *)->response = create_packet(response);
+        printf("sr 2\n");
         if (CAST(task->content, t_task *)->response->type == TYPE_T_RESPONSE_SUCCESS)
-            queue_enqueue(&CAST(slave, t_slave *)->cluster->computation.done_queue, task);
+        {
+            printf("\tsuccessful task execution\n");
+            printf(">> %s\n", CAST(slave, t_slave *)->cluster->computation.);
+            queue_enqueue(&(CAST(slave, t_slave *)->cluster->computation.done_queue), task);
+        }
         else
         {
+            printf("\tfailed task execution\n");
             //free failure response
             queue_enqueue(&CAST(slave, t_slave *)->cluster->computation.tasks_queue, task);
         }
+        printf("sr 4\n");
         queue_dequeue(&CAST(slave, t_slave *)->tasks_queue, NULL);
+        printf("sr 3\n");
         // if (CAST(slave, t_slave *)->tasks_queue.size <
             // CAST(slave, t_slave *)->cluster->least_used_slave->tasks_queue.size)
             // CAST(slave, t_slave *)->cluster->least_used_slave = slave;
@@ -90,7 +108,19 @@ int connect_slaves(t_cluster *cluster)
             continue ;
         }
         DEBUG("\ta node is connected!..\n");
-        err = ERROR_WRAPPER(pthread_create(&tid, NULL, slave_routine, &cluster->nodes[i]));
+        err = ERROR_WRAPPER(pthread_create(&tid, NULL, slave_routine, &(cluster->nodes[i])));
+        // //
+        t_packet request;
+
+        request.type = TYPE_T_REQUEST_INIT;
+        err = ERROR_WRAPPER((request.data = read_file(cluster->program, &request.size)) == NULL);
+        if (!err)
+        {
+            queue_enqueue(&cluster->nodes[i].tasks_queue,
+                t_dstruct_create_node(create_task((t_task){create_packet(request), NULL}), sizeof(t_task)));
+        }
+        // //
+
         cluster->nodes[i].socket = err ? -1 : slave_socket;
     }
     return (0);
@@ -143,8 +173,10 @@ int init_cluster(char *configuration_file, t_cluster *cluster)
     err = ERROR_WRAPPER(connect_slaves(cluster) != 0);
     if (!err)
         init_computation(cluster);
+    /*
     err = ERROR_WRAPPER(init_slaves(cluster) != 0);
     err = ERROR_WRAPPER(pthread_create(&tid, NULL, cluster_loadbalancer, cluster) != 0);
+    */
     return (err);
 }
 
